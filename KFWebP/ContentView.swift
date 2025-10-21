5aecc530a5ae8fc76a60929cbc22d40d654c8112
vicgillx <johnvoid64@gmail.com>
2025-10-21 10:09:31 +0800

    Initial Commit

---
+++ KFWebP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KFWebP
+//
+//  Created by jsy on 21/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KFWebP/KFWebPApp.swift
@@ -0,0 +1,17 @@
+//
+//  KFWebPApp.swift
+//  KFWebP
+//
+//  Created by jsy on 21/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct KFWebPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
