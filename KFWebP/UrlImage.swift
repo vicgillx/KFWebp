ba5cfcd191fe18740e05efc2890ab51bc9185f9b
vicgillx <johnvoid64@gmail.com>
2025-10-21 11:04:52 +0800

    init

---
+++ KFWebP/UrlImage.swift
@@ -0,0 +1,18 @@
+//
+//  UrlImage.swift
+//  KFWebP
+//
+//  Created by jsy on 21/10/2025.
+//
+
+import SwiftUI
+
+struct UrlImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UrlImage()
+}
